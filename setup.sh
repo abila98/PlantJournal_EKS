b150b2918fb30c174d2c91b6ebc0d5cfa203c627
root <root@ip-10-0-1-229.us-west-1.compute.internal>
2026-07-02 10:27:43 +0000

    Added new files for pod identity association

--- setup.sh
+++ setup.sh
@@ -63,7 +63,7 @@ sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
 
 #Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl && rm kubectl
 kubectl version --client
 
 #Install helm
